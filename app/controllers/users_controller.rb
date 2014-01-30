a7ad14188cb6a8454d860251724d2a623d9dd5b9
Prescott Murphy <prurph@gmail.com>
2014-01-30 18:45:28 -0500

    Remove old user#create functionality that is now handled by devise. Merge into master

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,20 +7,21 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def new
-    @user = User.new
-  end
+  # Devise handles these now
+  # def new
+  #   @user = User.new
+  # end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      flash[:notice] = "User created!"
-      redirect_to action: :index
-    else
-      flash.now[:error] = @user.errors.full_messages.join(', ')
-      render :new
-    end
-  end
+  # def create
+  #   @user = User.new(user_params)
+  #   if @user.save
+  #     flash[:notice] = "User created!"
+  #     redirect_to action: :index
+  #   else
+  #     flash.now[:error] = @user.errors.full_messages.join(', ')
+  #     render :new
+  #   end
+  # end
 
   private
   def user_params
